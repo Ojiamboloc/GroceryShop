5a548fd3385f8e3864e6d6c31a739650837f465d
Ojiamboloc <ojiamboloc@gmail.com>
2022-12-22 00:45:37 -0800

    Initial Commit

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = ({title}) => {
+ /*   const headerStyle={
+
+        backgroundColor:'royalblue',
+        color:'#fff'
+    }*/
+  return (
+    <header>
+      <h1>{title}</h1>
+
+    </header>
+
+  )
+}
+Header.defaultProps={title:"Default Title" }
+export default Header
